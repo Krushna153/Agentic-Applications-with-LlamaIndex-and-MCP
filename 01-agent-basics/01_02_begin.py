92a55dd978a6159217487e6da28ec1ccab107ba4
Krushna153 <kmshah153@gmail.com>
2026-07-14 17:31:52 -0400

    Building a basic agent

--- 01-agent-basics/01_02_begin.py
+++ 01-agent-basics/01_02_begin.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from typing import Optional
 from llama_index.core.tools import FunctionTool
-from llama_index.core.agent.workflow import FunctionAgent
+from llama_index.core.agent.workflow import FunctionAgent, AgentWorkflow
 from llama_index.llms.openai import OpenAI
 from datasets import load_dataset
 from llama_index.core import Document, VectorStoreIndex
@@ -10,7 +10,7 @@ from llama_index.core.memory import Memory
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(".env.example")
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -27,11 +27,32 @@ def create_query_engine():
 
 query_engine = create_query_engine()
 
-
-
+def search_e_commerce_dataset(query: str) -> str:
+    # Useful to search through clothing items and prices in an e-commerce dataset
+    response = query_engine.query(query)
+    return response.response
 
 async def main():
-    pass
+    llm = OpenAI(model="gpt-4.1-mini", api_key=OPENAI_API_KEY)
+    agent_prompt = """
+    You are a helpful e-commerce assistant. You have access to a tool that allows you to search through a dataset of clothing items and their prices. Use this tool to answer user questions or seacrh through clothing items and prices in an e-commerce dataset. If the user asks a question that requires searching through the dataset, use the tool to find the answer. If the user asks a question that does not require searching through the dataset, answer the question directly.
+    """
+
+    memory = Memory.from_defaults(token_limit=40000)
+
+    # Define your agent based on the LLM, tools, and system prompt
+    agent = FunctionAgent(llm=llm, 
+                          tools=[search_e_commerce_dataset], 
+                          system_prompt=agent_prompt)
+    
+    while True:
+        user_input = input("Enter your query: ")
+        if user_input.lower() in ["exit", "quit"]:
+            print("Exiting the agent. Goodbye!")
+            break
+        
+        response = await agent.run(user_input, memory=memory)
+        print(f"Agent: {response}")
 
 if __name__ == "__main__":
     asyncio.run(main())
